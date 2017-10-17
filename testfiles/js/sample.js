285d6a9234e5de196954c83ef0824c21d2e9c1c8
HyrumClark <31934225+HyrumClark@users.noreply.github.com>
2017-10-17 11:55:28 -0600

    tests

--- testfiles/js/sample.js
+++ testfiles/js/sample.js
@@ -1,1 +1,11 @@
 alert("You are entering the test page for Hyrum Clark. You may find that this page is not complete or has a lot of weird stuff going on. Continue at your own risk.");
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
