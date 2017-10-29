7af9d581bb41c3ef033a1b4bc90eb413fc95c2d6
HyrumClark <31934225+HyrumClark@users.noreply.github.com>
2017-10-28 20:18:23 -0600

    Revert "Added Gallery"
    This reverts commit 5f3127c5a54ca5f8b3cdc02b756e8ee18dad0f24.

--- cit230/assignments/lesson-7/scripts/windchill.js
+++
@@ -1,6 +0,0 @@
-var temperature = 60;
-var speed = 10;
-var chill = 35.74 + 0.6215 * temperature - 35.75 * (Math.pow(speed, 0.16)) + 0.4275 * temperature * (Math.pow(speed, 0.16));
-var rounded = Math.round(chill * 10) /10;
-
-document.getElementById("windchill").innerHTML = rounded;
